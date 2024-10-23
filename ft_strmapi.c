aebdd379f9c1e5295dd0bfcd82e02e07e7df58b9
Ayse Nur Gedikoglu <gedikoglu_27@icloud.com>
2024-10-23 15:05:07 +0300

    split add

---
+++ ft_strmapi.c
@@ -0,0 +1,19 @@
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*str;
+
+	if (!s || !f)
+		return (NULL);
+	if (!(str = malloc(sizeof(char) * (ft_strlen(s) + 1))))
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = 0;
+	return (str);
+}
