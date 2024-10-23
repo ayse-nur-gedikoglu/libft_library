ddf6b18aa2aaf470336f4c3d342bf6308389839a
Ayse Nur Gedikoglu <gedikoglu_27@icloud.com>
2024-10-23 14:23:26 +0300

    fonksiyonlar eklendi

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,6 +1,31 @@
 #include "stdlib.h"
 
+void to_uppercase(unsigned int index, char *c);
+
 void ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-    
+    unsigned int i;
+
+    i = 0;
+    if (!s || !f)
+        return;
+    while (s[i])
+    {
+        (*f)(i, s + i); // Hem index 'i' hem de 's + i' yani adresi fonksiyona gönderiyoruz.
+        i++;
+
+    }
+}
+void to_uppercase(unsigned int index, char *c)
+{
+    if (*c >= 'a' && *c <= 'z')
+        *c = *c - 32;
+}
+
+int main(void)
+{
+    char str[] = "hello world!";
+    ft_striteri(str, to_uppercase);
+    printf("%s\n", str);  // "HELLO WORLD!" yazdırır
+    return 0;
 }
